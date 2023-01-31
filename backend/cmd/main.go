64d46144ae2b4da75012d3d5c5716abada915e45
top123alt <top123@mail.top>
2023-01-31 21:36:24 +0700

    Добавлен запуск сервера и тестовое наполнение обработчиков

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"log"
 	"todos/config"
+	"todos/internal/handler"
+	"todos/internal/repository"
+	"todos/internal/server"
+	"todos/internal/service"
 )
 
 func main() {
 	cfg, err := config.InitConfig("../config")
 	if err != nil {
-		log.Fatal("Ошибка инициализации", err)
+		log.Fatal("Ошибка инициализации ", err)
 	}
-	fmt.Println(cfg.Server.Host)
-	fmt.Println(cfg.DB.Name)
-	//server.Run(cfg)
+
+	// Внедрение зависимостей
+	repo := repository.NewRepository()
+	service := service.NewService(repo)
+	handler := handler.NewHandler(service)
+
+	fmt.Printf("Попытка запуска сервера на: %s:%s\n", cfg.Server.Host, cfg.Server.Port)
+
+	if err := server.Run(cfg, handler.InitRoutes()); err != nil {
+		log.Fatal("Ошибка запуска сервера", err)
+	}
 }
